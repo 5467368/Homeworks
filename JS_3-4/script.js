5115bdf68f6862a3086b37d4988c6a615025e6f5
Miroslav Chulskiy <5467368@gmail.com>
2016-03-13 00:31:21 +0200

    Full version without CSS

--- JS_3-4/script.js
+++ JS_3-4/script.js
@@ -3,18 +3,53 @@ var newElement = document.createElement('div');
 newElement.className = "wrapper";
 document.body.appendChild(newElement);
 
-var parent = document.body.getElementsByClassName('wrapper');
+var wrapper = document.body.querySelector('.wrapper');
 newElement = document.createElement('h3');
 newElement.innerHTML = 'Тест по программированию';
-document.body.appendChild(newElement);
+wrapper.appendChild(newElement);
 
 newElement = document.createElement('form');
 newElement.setAttribute('name', 'test');
 newElement.setAttribute('method', 'GET');
 newElement.setAttribute('action', '');
-document.body.appendChild(newElement);
+wrapper.appendChild(newElement);
 
-parent = document.getElementsByTagName('form');
-newElement = document.createElement('p');
-newElement.innerHTML = '1. Вопрос №1';
-document.parent.appendChild(newElement);
+//Вставляем теги <UL>
+var form = wrapper.querySelector('form');
+for(var i=1; i<4; i++){
+  newElement = document.createElement('p');
+  newElement.innerHTML = i+'. Вопрос №'+i;
+  form.appendChild(newElement);
+
+  newElement = document.createElement('ul');
+  form.appendChild(newElement);
+
+//Вставляем в <UL> теги <LI>
+  var ul = form.querySelectorAll('ul');
+  for(var j=1; j<4; j++){
+    newElement = document.createElement('li');
+    newElement.style.listStyle = 'none';
+    ul[i-1].appendChild(newElement);
+
+    var li = ul[i-1].querySelectorAll('li');
+    newElement = document.createElement('input');
+    newElement.setAttribute('type', 'checkbox');
+    newElement.setAttribute('name', 'an_'+i+'-'+j);
+    newElement.setAttribute('id', 'an_'+i+'-'+j);
+    li[j-1].appendChild(newElement);
+
+    newElement = document.createElement('label');
+    newElement.setAttribute('for', 'an_'+i+'-'+j);
+    newElement.innerHTML = 'Вариант ответа №'+j;
+    newElement.style.marginLeft = '5px';
+    li[j-1].appendChild(newElement);
+
+    newElement = document.createElement('br');
+    li[j-1].appendChild(newElement);
+  }
+}
+
+newElement = document.createElement('button');
+newElement.setAttribute('type', 'submit');
+newElement.innerHTML = 'Проверить мои результаты';
+form.appendChild(newElement);
