cc5286f9e896e15471ad30501baaeae7a4979194
Miroslav Chulskiy <5467368@gmail.com>
2016-03-21 13:03:41 +0200

    Оптимизировано

--- JS_7-8/script.js
+++ JS_7-8/script.js
@@ -14,49 +14,11 @@ $(function() {
   };
 
   $wrapper = $('.wrapper');
-  $wrapper.css({
-    margin: '0 auto',
-    width: '1000px'
-  });
-
   $section = $('.section');
-  $section.css({
-    width: '500px',
-    height: 'auto',
-    border: '1px solid black',
-    borderRadius: '4px',
-    backgroundColor: 'white',
-    margin: '50px auto',
-    fontSize: '15px'
-  });
-
   $menu = $('.menu');
-  $menu.css({
-    width: '99%',
-    height: '30px',
-    backgroundColor: '#cccccc',
-    border: '1px solid #aaaaaa',
-    borderRadius: '4px',
-    margin: '3px auto 0 auto'
-  });
-
   $ul = $('ul');
-
   $li = $('li');
-  $li.css({
-    listStyle: 'none',
-    // display: 'inline-block',
-    float: 'left',
-    padding: '4px 5px',
-    backgroundColor: '#e6e6e6',
-    border: '1px solid #D3D3D3',
-    borderBottom: 'none',
-    borderTopRightRadius: '4px',
-    borderTopLeftRadius: '4px',
-    margin: '4px 0 0 3px',
-    cursor: 'pointer'
-  });
-  
+
   start();
 
   $div = $('article');
@@ -64,37 +26,31 @@ $(function() {
     padding: '15px'
   });
 
-  // var $liArr = $('li');
-  // $liArr = $.makeArray($liArr);
-  // for (var i = 0; i < $liArr.length; i++) {
-  //   console.log($liArr[i]);
-  // }
-
-    $li.eq(0).on('click', function(){
-      $(this).css({
-        cursor: 'default',
-        backgroundColor: 'white',
-        borderBottom: 'white',
-        paddingBottom: '5px'
-      });
-      $li.eq(1).css({
-          paddingBottom: 'px',
-          cursor: 'pointer',
-          backgroundColor: '#e6e6e6',
-          border: '1px solid #D3D3D3',
-          borderBottom: 'none',
-      });
-      $li.eq(2).css({
-          paddingBottom: 'px',
-          cursor: 'pointer',
-          backgroundColor: '#e6e6e6',
-          border: '1px solid #D3D3D3',
-          borderBottom: 'none',
-      });
-      $('article').eq(0).show();
-      $('article').eq(1).hide();
-      $('article').eq(2).hide();
+  $li.eq(0).on('click', function(){
+    $(this).css({
+      cursor: 'default',
+      backgroundColor: 'white',
+      borderBottom: 'white',
+      paddingBottom: '5px'
     });
+    $li.eq(1).css({
+      paddingBottom: 'px',
+      cursor: 'pointer',
+      backgroundColor: '#e6e6e6',
+      border: '1px solid #D3D3D3',
+      borderBottom: 'none',
+    });
+    $li.eq(2).css({
+      paddingBottom: 'px',
+      cursor: 'pointer',
+      backgroundColor: '#e6e6e6',
+      border: '1px solid #D3D3D3',
+      borderBottom: 'none',
+    });
+    $('article').eq(0).show();
+    $('article').eq(1).hide();
+    $('article').eq(2).hide();
+  });
 
 
     $li.eq(1).on('click', function(){
@@ -169,18 +125,6 @@ $(function() {
       }
     }
   );
-  // $li.each(function(i){
-  //   $(this).on('click', function(){
-  //     $(this).css({
-  //       cursor: 'default',
-  //       backgroundColor: 'white',
-  //       borderBottom: 'white',
-  //       paddingBottom: '5px'
-
-  //     });
-  //   }); 
-  // });
-
 });
 
 
--- JS_7-8/script1.js
+++ JS_7-8/script1.js
@@ -1,68 +1,43 @@
-$('.wrapper').css({
-  margin: '0 auto',
-  width: '600px'
-})
-
-$('form').css({
-  width: '100%',
-  height: '200px',
-  margin: '50px auto'
-})
-
-$('fieldset').css({
-  width: '100%',
-  height: 'auto',
-  padding: '10px',
-  margin: '0 auto'
-});
-
-$(button).css({
-  borderRadius: '5px',
-  height: '30px',
-  width: '90px',
-  border: '1px solid #d3d3d3',
-  background: '#e6e6e6'
-})
-
-var toolTip = ['<span>Please provide your firstname.</span>', '<span>Please provide also your lastname.</span>', '<span>Your home or work address.</span>'];
-
 var $label = $('label');
+var $input = $('input');
+var $div = $('div');
+var $span = $('span');
+
 for (var i = 0; i < 3; i++) {
-
-
   $label.eq(i).css({
     display: 'inline-block',
     width: function(){ return $label.eq(0).css('width') },
   });
-  
-  
+
 };
 
-$('input').eq(0).hover(
+$input.eq(0).hover(
     function(){
-      $('span').remove();
-      $('div').eq(0).append(toolTip[0]);
+      $span.hide();
+      $span.eq(0).show();
     },
-    function(){ $('span').remove(); }
-  );
-$('input').eq(1).hover(
+    function(){ $span.hide(); }
+);
+
+$input.eq(1).hover(
     function(){
-      $('span').remove();
-      $('div').eq(1).append(toolTip[1]);
+      $span.hide();
+      $span.eq(1).show();
     },
-    function(){ $('span').remove(); }
-  );
-$('input').eq(2).hover(
+    function(){ $span.hide(); }
+);
+
+$input.eq(2).hover(
     function(){
-      $('span').remove();
-      $('div').eq(2).append(toolTip[2]);
+      $span.hide();
+      $span.eq(2).show();
     },
-    function(){ $('span').remove(); }
-  );
+    function(){ $span.hide(); }
+);
 
 $(button).on('click', function(){
   for (var i = 0; i < 3; i++) {
-    $('span').eq(i).remove();
-    $('div').eq(i).append(toolTip[i]);
+    $span.eq(i).hide();
+    $span.eq(i).show();
   }
 });
