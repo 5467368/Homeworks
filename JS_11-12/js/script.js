68f18f81a4d21e243f3982c3e12bf8eb4ba1d2eb
5467368 <5467368@gmail.com>
2016-04-03 18:31:26 +0300

    Добавлен шаблонизатор John Resig

--- JS_11-12/js/script.js
+++ JS_11-12/js/script.js
@@ -1,5 +1,23 @@
 $(function(){
 	$('.wrapper').myCarousel({
-     		carouselCircle: true
+     		// carouselCircle: true
      	});
+});
+
+$(function(){
+
+	var $tmplHTML = $('#tmplscript').html();
+	var data = {
+		name: 'Чульский Мирослав Александрович',
+		image: 'img/img.jpg',
+		text1: 'Пытаюсь учиться на курсах GoForIT',
+		li1: 'Мне так нравиться',
+		li2: 'Я так хочу',
+		li3: 'Просто так',
+		phone: '+38-050-5467368',
+		profile: 'https://www.facebook.com/profile.php?id=100002129083915',
+		text4: 'Чаще улыбайтесь )))'
+	};
+	var newContent = tmpl($tmplHTML, data);
+	$('.tamplate').append(newContent);
 });
