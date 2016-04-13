cc3ecaa549e73773cc7d614ed3ac2115bb6b14b7
5467368 <5467368@gmail.com>
2016-04-13 18:46:07 +0300

    Added files via upload

--- JS_15-16/script.js
+++ JS_15-16/script.js
@@ -1,14 +1,14 @@
 var searchRes;
 function GoogleCallback(func, data){
-    console.log (data);
+    searchRes = data.results;
+    
 };
-// function GoogleCallback (func, data) {
-//     window[func](data);
-// }
+console.log(searchRes);
 
 $(function() {
 
-	// $.getJSON("http://ajax.googleapis.com/ajax/services/search/web?v=1.0?key=ABQIAAAACKQaiZJrS0bhr9YARgDqUxQBCBLUIYB7IF2WaNrkYqF0tBovNBQFDtM_KNtb3xQxWff2mI5hipc3lg&q=" + + "&callback=GoogleCallback&context=?",
+
+	// $.getJSON("http://ajax.googleapis.com/ajax/services/search/web?v=1.0?q=" + "ggg" + "&callback=GoogleCallback&context=?",
 	// 	function(data){
  //   	 var ul = document.createElement("ul");
  //    	$.each(data.results, function(i, val){
@@ -19,37 +19,38 @@ $(function() {
  //        $('.searchres').html(ul);
  //    });
 
-	var url1 = 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0?key=ABQIAAAACKQaiZJrS0bhr9YARgDqUxQBCBLUIYB7IF2WaNrkYqF0tBovNBQFDtM_KNtb3xQxWff2mI5hipc3lg&q=';
-    var url2 = '&callback=GoogleCallback&context=?';
+
+
+	var url1 = "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=";
+    var url2 = "&callback=GoogleCallback&context=?";
 	$('#inputbox').keydown(function(e) {
         var searchText;
 		if (e.which == 13) {
             searchText = $('#input-form').val();
 			$.ajax({
 				url: url1 + searchText + url2,
-				// method: 'GET',
+				method: 'GET',
 				dataType: 'jsonp'
 				// success: function(){}
 				// error: function(){}
 			})
             // .done(function(){
-                
+           outputSearchRes();     
             // });
 		}
 	});
 
-	
-// 	function error(){};
-// 	function success(){};
+	function outputSearchRes() {
+        var ul = document.createElement("ul");
+        searchRes.each(function() {
+          var li = document.createElement("li");
+          li.innerHTML = '<a href="'+this.url+'" title="'+this.url+'" target="_blank">'+this.title+"</a> - "+this.content;                            
+          ul.appendChild(li);
+        });
+        $('.searchres').html(ul);
+    };
+// // 	function error(){};
+// // 	function success(){};
+    // 
 
-	// function outputSearchRes(data){
-	// 	console.log (data);
-	// 	var ul = document.createElement("ul");
-	// 	$.each(data.results, function(i, val){
- //      var li = document.createElement("li");
- //      li.innerHTML = '<a href="'+val.url+'" title="'+val.url+'" target="_blank">'+val.title+"</a> - "+val.content;                            
- //      ul.appendChild(li);
- //    });
- //    $('.searchres').html(ul);
-	// };
 });
