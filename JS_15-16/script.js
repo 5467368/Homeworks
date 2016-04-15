b583645e2ec674effe551dfb71c841a41f198b3b
5467368 <5467368@gmail.com>
2016-04-15 20:06:02 +0300

    Оба задания
    Index.html - это задание с поиском
    proto - задание с прототипами

--- JS_15-16/script.js
+++ JS_15-16/script.js
@@ -1,24 +1,10 @@
-var searchRes;
-function GoogleCallback(func, data){
-    searchRes = data.results;
-};
-console.log(searchRes);
+'use strict';
+
+function GoogleCallback (func, data) {
+    window[func](data);
+}
 
 $(function() {
-
-
-	// $.getJSON("http://ajax.googleapis.com/ajax/services/search/web?v=1.0?q=" + "ggg" + "&callback=GoogleCallback&context=?",
-	// 	function(data){
- //   	 var ul = document.createElement("ul");
- //    	$.each(data.results, function(i, val){
- //            var li = document.createElement("li");
- //            li.innerHTML = '<a href="'+val.url+'" title="'+val.url+'" target="_blank">'+val.title+"</a> - "+val.content;                            
- //            ul.appendChild(li);
- //    	});
- //        $('.searchres').html(ul);
- //    });
-
-
 
 	var url1 = "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=";
     var url2 = "&callback=GoogleCallback&context=?";
@@ -26,30 +12,25 @@ $(function() {
         var searchText;
 		if (e.which == 13) {
             searchText = $('#input-form').val();
-			$.ajax({
-				url: url1 + searchText + url2,
-				method: 'GET',
-				dataType: 'jsonp'
-				// success: function(){}
-				// error: function(){}
-			})
-            // .done(function(){
-           outputSearchRes();     
-            // });
-		}
-	});
-
-	function outputSearchRes() {
-        var ul = document.createElement("ul");
-        searchRes.each(function() {
-          var li = document.createElement("li");
-          li.innerHTML = '<a href="'+this.url+'" title="'+this.url+'" target="_blank">'+this.title+"</a> - "+this.content;                            
-          ul.appendChild(li);
-        });
-        $('.searchres').html(ul);
-    };
-// // 	function error(){};
-// // 	function success(){};
-    // 
-
+            var url = url1 + searchText + url2;
+			// $.ajax({
+			// url: url1 + searchText + url2,
+			// 	method: 'GET',
+			// 	dataType: 'jsonp'
+			// 	// success: function(){}
+			// 	// error: function(){}
+			// });
+            $.getJSON(url, {rsz:8},
+                function(data){
+                    var ul = document.createElement("ul");
+                    $.each(data.results, function(i, val){
+                        var li = document.createElement("li");
+                        li.innerHTML = '<br><a href="'+val.url+'" title="'+val.url+'" target="_blank">'+val.title+'</a><p class="url">' + val.url + "</p>" + val.content + '<br>';
+                        ul.appendChild(li);
+                    });
+                    $('.searchres').html(ul);
+                }
+            );
+        }
+    });
 });
